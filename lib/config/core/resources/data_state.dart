04093c8f5475f2753c94285c985bb37abed9b900
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2023-07-21 23:13:22 +0500

    Setting up the daily_news feature

---
+++ lib/config/core/resources/data_state.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+// Very handy when we are doing network calls to get wrap network calls later to get successfull response or a failed one
+
+abstract class DataState<T> {
+  final T? data;
+  final DioError? error;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioError error) : super(error: error);
+}
